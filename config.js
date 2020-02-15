d72804175cb77da87c6e27e0d8e2683f340f588f
snailuncle <1789500304@qq.com>
2020-02-16 03:02:01 +0800

    daily-02-16-界面中添加变量

--- config.js
+++ config.js
@@ -6,5 +6,7 @@ module.exports = {
     bark: service.bark,
     goToBed: service.goToBed,
     readNum: service.readNum,
-  }
+  },
+  celebrityName:'哲学家: 伯特兰·罗素',
+  celebrityImg:'https://gss0.bdstatic.com/94o3dSag_xI4khGkpoWK1HF6hhy/baike/crop%3D0%2C0%2C550%2C363%3Bc0%3Dbaike80%2C5%2C5%2C80%2C26/sign=f9f13c559482d158afcd03f1bd3a35e8/7e3e6709c93d70cf133f10b6f0dcd100bba12b47.jpg',
 }
