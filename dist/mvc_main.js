556b312a16bebf36cef1adb3fc698aa9d4a417e5
snailuncle <1789500304@qq.com>
2020-02-17 19:24:57 +0800

    1

---
+++ dist/mvc_main.js
@@ -0,0 +1,135 @@
+"ui";
+;
+;
+;
+;
+
+var _0x1eac = [
+    '哲学家:\x20伯特兰·罗素',
+    'https://gss0.bdstatic.com/94o3dSag_xI4khGkpoWK1HF6hhy/baike/crop%3D0%2C0%2C550%2C363%3Bc0%3Dbaike80%2C5%2C5%2C80%2C26/sign=f9f13c559482d158afcd03f1bd3a35e8/7e3e6709c93d70cf133f10b6f0dcd100bba12b47.jpg',
+    'log',
+    '我在吃\x20\x20方便面',
+    '房租还没交,\x20忧桑',
+    '我要\x20\x20睡觉了',
+    'num',
+    'button',
+    'click',
+    'call',
+    'exports',
+    'defineProperty',
+    'undefined',
+    'toStringTag',
+    'Module',
+    '__esModule',
+    'create',
+    'default',
+    'prototype',
+    'eat',
+    'bark',
+    'readNum'
+];
+(function (_0x9009ef, _0x1a399a) {
+    var _0x45d7b0 = function (_0x2678eb) {
+        while (--_0x2678eb) {
+            _0x9009ef['push'](_0x9009ef['shift']());
+        }
+    };
+    _0x45d7b0(++_0x1a399a);
+}(_0x1eac, 0x195));
+var _0x1813 = function (_0xd4f724, _0x5579cf) {
+    _0xd4f724 = _0xd4f724 - 0x0;
+    var _0x3e26b2 = _0x1eac[_0xd4f724];
+    return _0x3e26b2;
+};
+!function (_0x2dfcdf) {
+    var _0x1c6c91 = {};
+    function _0x2836ca(_0x2a1c5c) {
+        if (_0x1c6c91[_0x2a1c5c])
+            return _0x1c6c91[_0x2a1c5c]['exports'];
+        var _0xe8c667 = _0x1c6c91[_0x2a1c5c] = {
+            'i': _0x2a1c5c,
+            'l': !0x1,
+            'exports': {}
+        };
+        return _0x2dfcdf[_0x2a1c5c][_0x1813('0x0')](_0xe8c667[_0x1813('0x1')], _0xe8c667, _0xe8c667[_0x1813('0x1')], _0x2836ca), _0xe8c667['l'] = !0x0, _0xe8c667['exports'];
+    }
+    _0x2836ca['m'] = _0x2dfcdf, _0x2836ca['c'] = _0x1c6c91, _0x2836ca['d'] = function (_0x2dfcdf, _0x1c6c91, _0x3f7c44) {
+        _0x2836ca['o'](_0x2dfcdf, _0x1c6c91) || Object[_0x1813('0x2')](_0x2dfcdf, _0x1c6c91, {
+            'enumerable': !0x0,
+            'get': _0x3f7c44
+        });
+    }, _0x2836ca['r'] = function (_0x2dfcdf) {
+        _0x1813('0x3') != typeof Symbol && Symbol[_0x1813('0x4')] && Object[_0x1813('0x2')](_0x2dfcdf, Symbol[_0x1813('0x4')], { 'value': _0x1813('0x5') }), Object[_0x1813('0x2')](_0x2dfcdf, _0x1813('0x6'), { 'value': !0x0 });
+    }, _0x2836ca['t'] = function (_0x2dfcdf, _0x1c6c91) {
+        if (0x1 & _0x1c6c91 && (_0x2dfcdf = _0x2836ca(_0x2dfcdf)), 0x8 & _0x1c6c91)
+            return _0x2dfcdf;
+        if (0x4 & _0x1c6c91 && 'object' == typeof _0x2dfcdf && _0x2dfcdf && _0x2dfcdf['__esModule'])
+            return _0x2dfcdf;
+        var _0x3f9761 = Object[_0x1813('0x7')](null);
+        if (_0x2836ca['r'](_0x3f9761), Object[_0x1813('0x2')](_0x3f9761, _0x1813('0x8'), {
+                'enumerable': !0x0,
+                'value': _0x2dfcdf
+            }), 0x2 & _0x1c6c91 && 'string' != typeof _0x2dfcdf)
+            for (var _0x3b83bd in _0x2dfcdf)
+                _0x2836ca['d'](_0x3f9761, _0x3b83bd, function (_0x1c6c91) {
+                    return _0x2dfcdf[_0x1c6c91];
+                }['bind'](null, _0x3b83bd));
+        return _0x3f9761;
+    }, _0x2836ca['n'] = function (_0x2dfcdf) {
+        var _0x1c6c91 = _0x2dfcdf && _0x2dfcdf[_0x1813('0x6')] ? function () {
+            return _0x2dfcdf[_0x1813('0x8')];
+        } : function () {
+            return _0x2dfcdf;
+        };
+        return _0x2836ca['d'](_0x1c6c91, 'a', _0x1c6c91), _0x1c6c91;
+    }, _0x2836ca['o'] = function (_0x2dfcdf, _0x1c6c91) {
+        return Object[_0x1813('0x9')]['hasOwnProperty'][_0x1813('0x0')](_0x2dfcdf, _0x1c6c91);
+    }, _0x2836ca['p'] = './', _0x2836ca(_0x2836ca['s'] = 0x3);
+}([
+    function (_0x565450, _0x33c9ff, _0x39e657) {
+        let _0x4a38f0 = _0x39e657(0x1);
+        _0x565450[_0x1813('0x1')] = {
+            'button': {
+                'eat': _0x4a38f0[_0x1813('0xa')],
+                'bark': _0x4a38f0[_0x1813('0xb')],
+                'goToBed': _0x4a38f0['goToBed'],
+                'readNum': _0x4a38f0[_0x1813('0xc')]
+            },
+            'celebrityName': _0x1813('0xd'),
+            'celebrityImg': _0x1813('0xe')
+        };
+    },
+    function (_0x299fbd, _0x56899c) {
+        let _0x40f970 = function (_0x299fbd) {
+            toastLog(_0x299fbd), ui[_0x1813('0xf')]['setText'](_0x299fbd);
+        };
+        _0x299fbd[_0x1813('0x1')] = {
+            'eat': function () {
+                _0x40f970(_0x1813('0x10'));
+            },
+            'bark': function () {
+                _0x40f970(_0x1813('0x11'));
+            },
+            'goToBed': function () {
+                _0x40f970(_0x1813('0x12'));
+            },
+            'readNum': function () {
+                let _0x299fbd = ui[_0x1813('0x13')]['text']();
+                _0x40f970(_0x299fbd);
+            }
+        };
+    },
+    function (_0x9db006, _0x54b586) {
+        _0x9db006[_0x1813('0x1')] = function (_0x9db006) {
+            let _0x54b586 = _0x9db006[_0x1813('0x14')];
+            for (var _0x140ef0 in _0x54b586)
+                ui[_0x140ef0]['on'](_0x1813('0x15'), _0x54b586[_0x140ef0]);
+        };
+    },
+    function (_0x5e705f, _0x2ac695, _0x10ad13) {
+        'use strict';
+        _0x10ad13['r'](_0x2ac695);
+        let _0x257305 = _0x10ad13(0x0);
+        ui['layout']('<vertical>\x0d\x0a\x20\x20<button\x20id=\x27eat\x27>eat</button>\x0d\x0a\x20\x20<button\x20id=\x27bark\x27>bark</button>\x0d\x0a\x20\x20<button\x20id=\x27goToBed\x27>goToBed</button>\x0d\x0a\x20\x20<input\x20id=\x27num\x27\x20w=\x22*\x22></input>\x0d\x0a\x20\x20<button\x20id=\x27readNum\x27>readNum</button>\x0d\x0a\x20\x20<text\x20id=\x27log\x27\x20gravity=\x27center\x27\x20textSize=\x2730sp\x27></text>\x0d\x0a\x20\x20<vertical>\x0d\x0a\x20\x20\x20\x20<text\x20id=\x27celebrityName\x27\x20gravity=\x27center\x27\x20textSize=\x2730sp\x27\x20text=\x27哲学家:\x20伯特兰·罗素\x27\x20textStyle=\x27bold\x27></text>\x0d\x0a\x20\x20\x20\x20<img\x20id=\x27celebrityImg\x27\x20src=\x27https://gss0.bdstatic.com/94o3dSag_xI4khGkpoWK1HF6hhy/baike/crop%3D0%2C0%2C550%2C363%3Bc0%3Dbaike80%2C5%2C5%2C80%2C26/sign=f9f13c559482d158afcd03f1bd3a35e8/7e3e6709c93d70cf133f10b6f0dcd100bba12b47.jpg\x27\x20></img>\x0d\x0a\x20\x20</vertical>\x0d\x0a</vertical>'), _0x10ad13(0x2)(_0x257305);
+    }
+]);
